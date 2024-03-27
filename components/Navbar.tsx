b8ccfb6fc6913fcd060059c38ac544a80bb18325
Jan Swiecioch <janjanswiecioch.com@gmail.com>
2024-03-27 09:35:33 +0100

    navbar links created

---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { CarFront, User } from "lucide-react"
+import Link from "next/link"
+import { Button } from "./ui/button"
+
+export const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between p-3">
+        <Link href={"/"} className="text-xl font-semibold">myAuto</Link>
+        <div className="flex gap-5 items-center">
+          <span className="flex">
+          <User/>
+          <Link href={"/login"}>Login</Link>
+          </span>
+          <Link href={"/car/create"}><Button className="flex gap-2"><CarFront />Sell your car</Button></Link>
+        </div>
+    </nav> 
+  )
+}
