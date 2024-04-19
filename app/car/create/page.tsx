671efb1c166eafe980aabd5d0a9a8d9dda18786b
PiotrRozczka <prozczka@outlook.com>
2024-04-19 21:53:24 +0200

    car creation form template

--- app/car/create/page.tsx
+++ app/car/create/page.tsx
@@ -1,9 +1,41 @@
-import { FC } from "react";
+"use client";
+import { getCarMakes } from "@/actions/getCarMakes";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { FormLabel } from "@/components/ui/form";
+import { FC, useEffect, useState } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>page</div>;
+  const [carMakes, setCarMakes] = useState<CarMake[]>([]);
+  const [carModels, setCarModels] = useState<CarModel[]>([]);
+
+  useEffect(() => {
+    getCarMakes().then((carMakes) => setCarMakes(carMakes));
+  }, []);
+
+  return (
+    <main className="flex w-screen justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sell your car</CardTitle>
+          <CardDescription>
+            Fill out this form with information that may interest your potential
+            buyers. Take your time to sell a car faster.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form></form>
+        </CardContent>
+      </Card>
+    </main>
+  );
 };
 
 export default page;
