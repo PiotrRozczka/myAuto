9757b474ee274dcf9a3fd2e99b4211edc5264477
PiotrRozczka <prozczka@outlook.com>
2024-04-21 16:21:47 +0200

    broken search action

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,7 @@ export default function Home() {
   return (
     <main className="flex w-screen items-center flex-col gap-10">
       <SearchCard />
-      <CarCard
-        city="Szczecin"
-        imageUrl="https://ocdn.eu/pulscms-transforms/1/2zuktkpTURBXy81NTMzZmJiZTczNWM2YzBlNDE0YTI0OWU1ZDY3MjRlNC5qcGeSlQPNAY_NAfDNBT_NAvSTBc0EsM0Cow"
-        name="BMW M2"
-        price={25000}
-        fuelType="Gas"
-        milage={200000}
-        gearBoxType="Authomatic"
-        productionYear={2006}
-      />
+
     </main>
   );
 }
---
+++ lib/validators/search.ts
@@ -0,0 +1,6 @@
+import { z } from 'zod'
+
+export const searchValidator = z.object({
+    make: z.string().optional(),
+    model: z.string().optional()
+})
